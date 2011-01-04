a649764e00fff2b9bb4e537a3f563521ae49c055
gruffypuffy <gruffypuffy@a095e5f5-aa9b-6458-2cbb-915481aa3ad0>
2011-01-04 20:01:39 +0000

    Integrated first version of PPM.

---
+++ src/ppm.h
@@ -0,0 +1,33 @@
+/*
+ *  HeliClone  - Copyright (C) 2010
+ *               Stefan Grufman <gruffypuffy <at> gmail.com>
+ *
+ *  Description: Replacement software for model radio:
+ *               FS-TH9X / Turnigy 9X / Eurgle 9X / iMax 9X.
+ *
+ *  Based on various projects for this radio:
+ *   - RadioClone v 0.01  - Copyright (C) 2010 Rafal Tomczak <rafit <at> m2p.pl>
+ *   - er9x -> http://code.google.com/p/er9x/
+ *  
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PPM_H_
+#define _PPM_H_
+
+extern void ppm_init(); 
+extern void ppm_reinit();
+extern void ppm_export(); 
+
+#endif // _PPM_H_
