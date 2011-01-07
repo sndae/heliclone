10065e457d30d198b8aab1543761408f2ed2f9ac
gruffypuffy <gruffypuffy@a095e5f5-aa9b-6458-2cbb-915481aa3ad0>
2011-01-07 11:30:21 +0000

    First (non-tested) attempt of curve-mixers...

--- src/template.c
+++ src/template.c
@@ -31,35 +31,6 @@ void template_simulator()
 {
 	strncpy(&(g_Model.name[0]), "Simulator", 10);
 
-	// 1-1 mixers...for simulator etc...
-	g_Model.mixers[0][0].type = MIX_DIRECT;
-	g_Model.mixers[0][0].input = MIX_IN_AIL;
-	g_Model.mixers[0][0].output = MIX_OUT_AILERON;
-	g_Model.mixers[0][0].scale = 100;
-	g_Model.mixers[0][0].condition = MIX_COND_TRUE;
-	g_Model.mixers[0][0].multiplex = MIX_REPLACE;
-
-	g_Model.mixers[0][1].type = MIX_DIRECT;
-	g_Model.mixers[0][1].input = MIX_IN_THR;
-	g_Model.mixers[0][1].output = MIX_OUT_THROTTLE;
-	g_Model.mixers[0][1].scale = 100;
-	g_Model.mixers[0][1].condition = MIX_COND_TRUE;
-	g_Model.mixers[0][1].multiplex = MIX_REPLACE;
-
-	g_Model.mixers[0][2].type = MIX_DIRECT;
-	g_Model.mixers[0][2].input = MIX_IN_ELE;
-	g_Model.mixers[0][2].output = MIX_OUT_ELEVATOR;
-	g_Model.mixers[0][2].scale = 100;
-	g_Model.mixers[0][2].condition = MIX_COND_TRUE;
-	g_Model.mixers[0][2].multiplex = MIX_REPLACE;
-
-	g_Model.mixers[0][3].type = MIX_DIRECT;
-	g_Model.mixers[0][3].input = MIX_IN_RUD;
-	g_Model.mixers[0][3].output = MIX_OUT_RUDDER;
-	g_Model.mixers[0][3].scale = 100;
-	g_Model.mixers[0][3].condition = MIX_COND_TRUE;
-	g_Model.mixers[0][3].multiplex = MIX_REPLACE;
-
 	// All servos NORMAL direction
 	g_Model.servoDirection = 0x00;
 
@@ -128,6 +99,102 @@ void template_simulator()
 	g_Model.curve[MDL_CURVE_PIT_HOLD][2] = 0;
 	g_Model.curve[MDL_CURVE_PIT_HOLD][3] = 50;
 	g_Model.curve[MDL_CURVE_PIT_HOLD][4] = 100;
+
+
+	// Simulator mixers...
+	g_Model.mixers[0][0].type = MIX_DIRECT;
+	g_Model.mixers[0][0].input = MIX_IN_AIL;
+	g_Model.mixers[0][0].output = MIX_OUT_AILERON;
+	g_Model.mixers[0][0].scale = 100;
+	g_Model.mixers[0][0].condition = MIX_COND_TRUE;
+	g_Model.mixers[0][0].multiplex = MIX_REPLACE;
+
+	g_Model.mixers[0][1].type = MIX_DIRECT;
+	g_Model.mixers[0][1].input = MIX_IN_ELE;
+	g_Model.mixers[0][1].output = MIX_OUT_ELEVATOR;
+	g_Model.mixers[0][1].scale = 100;
+	g_Model.mixers[0][1].condition = MIX_COND_TRUE;
+	g_Model.mixers[0][1].multiplex = MIX_REPLACE;
+
+	g_Model.mixers[0][2].type = MIX_DIRECT;
+	g_Model.mixers[0][2].input = MIX_IN_RUD;
+	g_Model.mixers[0][2].output = MIX_OUT_RUDDER;
+	g_Model.mixers[0][2].scale = 100;
+	g_Model.mixers[0][2].condition = MIX_COND_TRUE;
+	g_Model.mixers[0][2].multiplex = MIX_REPLACE;
+
+	// THROTTLE ID0
+	g_Model.mixers[0][3].type = MIX_CURVE;
+	g_Model.mixers[0][3].input = MIX_IN_THR;
+	g_Model.mixers[0][3].output = MIX_OUT_THROTTLE;
+	g_Model.mixers[0][3].scale = 100;
+	g_Model.mixers[0][3].condition = MIX_COND_ID0;
+	g_Model.mixers[0][3].multiplex = MIX_ADD;
+	g_Model.mixers[0][3].curve = MDL_CURVE_THR_ID0;
+
+	// THROTTLE ID1
+	g_Model.mixers[0][4].type = MIX_CURVE;
+	g_Model.mixers[0][4].input = MIX_IN_THR;
+	g_Model.mixers[0][4].output = MIX_OUT_THROTTLE;
+	g_Model.mixers[0][4].scale = 100;
+	g_Model.mixers[0][4].condition = MIX_COND_ID1;
+	g_Model.mixers[0][4].multiplex = MIX_ADD;
+	g_Model.mixers[0][4].curve = MDL_CURVE_THR_ID1;
+
+	// THROTTLE ID2
+	g_Model.mixers[0][5].type = MIX_CURVE;
+	g_Model.mixers[0][5].input = MIX_IN_THR;
+	g_Model.mixers[0][5].output = MIX_OUT_THROTTLE;
+	g_Model.mixers[0][5].scale = 100;
+	g_Model.mixers[0][5].condition = MIX_COND_ID2;
+	g_Model.mixers[0][5].multiplex = MIX_ADD;
+	g_Model.mixers[0][5].curve = MDL_CURVE_THR_ID2;
+
+	// THROTTLE HOLD
+	g_Model.mixers[0][6].type = MIX_CURVE;
+	g_Model.mixers[0][6].input = MIX_IN_THR;
+	g_Model.mixers[0][6].output = MIX_OUT_THROTTLE;
+	g_Model.mixers[0][6].scale = 100;
+	g_Model.mixers[0][6].condition = MIX_COND_THR;
+	g_Model.mixers[0][6].multiplex = MIX_REPLACE;
+	g_Model.mixers[0][6].curve = MDL_CURVE_THR_HOLD;
+
+	// PITCH ID0
+	g_Model.mixers[0][7].type = MIX_CURVE;
+	g_Model.mixers[0][7].input = MIX_IN_THR;
+	g_Model.mixers[0][7].output = MIX_OUT_PITCH;
+	g_Model.mixers[0][7].scale = 100;
+	g_Model.mixers[0][7].condition = MIX_COND_ID0;
+	g_Model.mixers[0][7].multiplex = MIX_ADD;
+	g_Model.mixers[0][7].curve = MDL_CURVE_PIT_ID0;
+
+	// PITCH ID1
+	g_Model.mixers[0][8].type = MIX_CURVE;
+	g_Model.mixers[0][8].input = MIX_IN_THR;
+	g_Model.mixers[0][8].output = MIX_OUT_PITCH;
+	g_Model.mixers[0][8].scale = 100;
+	g_Model.mixers[0][8].condition = MIX_COND_ID1;
+	g_Model.mixers[0][8].multiplex = MIX_ADD;
+	g_Model.mixers[0][8].curve = MDL_CURVE_PIT_ID1;
+
+	// PITCH ID2
+	g_Model.mixers[0][9].type = MIX_CURVE;
+	g_Model.mixers[0][9].input = MIX_IN_THR;
+	g_Model.mixers[0][9].output = MIX_OUT_PITCH;
+	g_Model.mixers[0][9].scale = 100;
+	g_Model.mixers[0][9].condition = MIX_COND_ID2;
+	g_Model.mixers[0][9].multiplex = MIX_ADD;
+	g_Model.mixers[0][9].curve = MDL_CURVE_PIT_ID2;
+
+	// PITCH HOLD
+	g_Model.mixers[0][10].type = MIX_CURVE;
+	g_Model.mixers[0][10].input = MIX_IN_THR;
+	g_Model.mixers[0][10].output = MIX_OUT_PITCH;
+	g_Model.mixers[0][10].scale = 100;
+	g_Model.mixers[0][10].condition = MIX_COND_THR;
+	g_Model.mixers[0][10].multiplex = MIX_REPLACE;
+	g_Model.mixers[0][10].curve = MDL_CURVE_PIT_HOLD;
+
 
 }
 
